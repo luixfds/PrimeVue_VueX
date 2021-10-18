3d24a8d8e5e3c05d51413f7af5752d97be4f9a58
luixfds <l.fds@yahoo.com>
2021-10-18 11:12:42 -0300

    up up

--- src/main.js
+++ src/main.js
@@ -3,4 +3,29 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import Primevue from 'primevue/config'
+
+
+import 'primevue/resources/themes/saga-purple/theme.css'       //theme
+import 'primevue/resources/primevue.min.css'                 //core css
+import 'primeicons/primeicons.css'                           //icons
+
+
+// COMPONENTES PRIMEVUE 
+import Calendar from 'primevue/calendar'
+import ColorPicker from 'primevue/colorpicker'
+import Password from 'primevue/password'
+import InputText from 'primevue/inputtext';
+
+
+const app = createApp(App);
+app.use(store);
+app.use(router);
+app.use(Primevue);
+app.component('Calendar', Calendar);
+app.component('ColorPicker', ColorPicker)
+app.component('Password', Password)
+app.component('InputText',InputText)
+
+
+app.mount('#app');
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,35 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="myform">
+      <h1>Meu Formulario</h1>
+      <span class="p-float-label">
+        <InputText id="username" type="text" v-model="value" />
+        <label for="username">Username</label>
+      </span>
+      <password /> 
+      <calendar />
+    </div>
   </div>
 </template>
+
+<style scoped lang="scss">
+
+  .about {
+    background-color: grey;
+    height: 88vh;
+    width: 99vw;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  .myform{
+    width: 40%;
+    height: 60%;
+    background-color: aliceblue;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+</style>
